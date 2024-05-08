a2de7dd2b6435d6601a21df50f86a750741b9a8b
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-05-09 00:26:15 +0200

    Fix bug in animate(), add coarse alpha and density calculator for a given interval

--- multifractal.py
+++ multifractal.py
@@ -8,6 +8,13 @@ import sys
 
 
 class Multifractal():
+    '''
+    Base class for a multifractal measure created using a multiplicative cascasde. 
+    The cascade is b-nomial (e.g. binomial,trinomial etc.) with the b multipliers defined in M. 
+    The set supporting the measure is Euclidean with the dimension E. 
+    Plotting is only supported with E=1. The values of the measure is stored in mu, 
+    and k denotes the number of iterations for the cascade for a given measure. 
+    '''
     def __init__(self, b, M, support_endpoints, E, k=0, mu=[1]):
         self.b = b
         if sum(M) == 1:
@@ -27,15 +34,24 @@ class Multifractal():
         
         
     def set_eps(self):
+        '''
+        Updates the number of iterations of a given instance, and updates scaling variable. 
+        '''
         self.k += 1
         self.eps = self.b**(-self.k)
        
     
     def set_support(self):
+        '''
+        Updates the x-ticks of the support set.
+        '''
         self.support = np.linspace(self.support_endpoints[0],self.support_endpoints[1],self.b**self.k,endpoint=False)
     
     
     def iterate(self, k):
+        '''
+        Does k iterations of the cascade, then Plots the resulting measure. 
+        '''
         while k > 0:
             self.set_eps()
             self.set_support()
@@ -49,16 +65,25 @@ class Multifractal():
         
         
     def check_measure(self):
+        '''
+        Returns the sum of all measures on the set. Should sum to 1 if we want a probablity measure.
+        '''
         return self.mu.sum()
     
     
     def plot_measure(self):
+        '''
+        Plots the multifractal on the whole set supporting it
+        '''
         fig, ax = plt.subplots()
         plot = ax.bar(np.linspace(0,1-(1/self.b**self.k),self.b**self.k),self.mu,1/(self.b**self.k),align='edge')
         plt.show()
             
     
     def convert_address(self,address):
+        '''
+        Converts a binary address, to an integer index.
+        '''
         if self.k >= len(address):
             x = 0
             for i in range(1,len(address)+1):
@@ -70,6 +95,9 @@ class Multifractal():
     
     
     def plot_interval(self,address):
+        '''
+        Plots the measure on a given inerval of the support. 
+        '''
         fig,ax = plt.subplots()
         start = self.convert_address(address)
         h = len(address)
@@ -80,25 +108,36 @@ class Multifractal():
         plot = ax.bar(np.linspace(self.support[start],end,self.b**(self.k-h),endpoint=False),self.mu[start:start+self.b**(self.k-h)],1/(self.b**self.k),align='edge')
         plt.show()
         
-        
-    def animate(self, frames):
+
+    def animate(self, frames, filename):
         fig, ax = plt.subplots()
-        
-        bars = ax.bar(0,1,1,align='edge')
-        
-        def animate_frame(frame):
-            ax.clear()
             
+        bars = ax.bar(np.linspace(0,1,self.b**frames,endpoint=False),np.ones(self.b**frames),1/(self.b**frames),align='edge')
+        
+        def update(frame):
+            for i, bar in enumerate(bars):
+                n = int(i // (self.b**frames / self.b**self.k))
+                bar.set_height(self.mu[n])
+                
+            ax.set_ylim([0,max(self.mu)+0.1*max(self.mu)])
             self.iterate(1)
 
-            while self.k > len(bars):
-                bars.append(ax.bar(0,1,1,align='edge'))
-                
-            for i, bar in enumerate(bars):
-                bar.set_height(self.mu[i])
-                bar.set_width(1/(self.b**self.k))
-                
             return bars
         
-        ani = animation.FuncAnimation(fig=fig, func=animate_frame, frames=frames, interval=120)
-        plt.show()
+        def init_func():
+            return bars
+        
+        ani = animation.FuncAnimation(fig=fig, func=update, frames=frames, interval=500, init_func=init_func)
+        ani.save(filename, writer="imagemagick")
+
+        
+    def coarse_alpha(self, address):
+        x = self.convert_address(address)
+        alpha = math.log(self.mu[x]) / math.log(self.eps)
+        return alpha
+    
+    
+    def coarse_density(self, address):
+        x = self.convert_address(address)
+        density = self.mu[x] / self.eps
+        return density
