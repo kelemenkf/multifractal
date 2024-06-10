af19d0b7cfe4e4375c0c980795b692d64b681313
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-06-10 12:20:19 +0200

    Make split data

--- detrended_fluctation_analysis.py
+++ detrended_fluctation_analysis.py
@@ -8,13 +8,17 @@ from repos.multifractal.multifractal import Multifractal
 class FluctuationAnalysis():
     def __init__(self, data, nu):
         self.data = data
+        self.diff_data = np.diff(data)
         self.nu = nu
-        self.N = data.size
-        self.s = self.N / self.nu
+        self.N = self.diff_data.size
+        self.s = self.N // self.nu
+
 
     def split_data(self):
-        self.data
+        split_data = [self.diff_data[i:i+self.s] for i in range(0,self.N,self.s)]
+        return np.array(split_data)
         
+
     def rescaled_range(self):
         pass
 
