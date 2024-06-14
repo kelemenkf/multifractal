a52d62361694be865fd10d7395d30c0b91adfa1c
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-06-14 10:59:56 +0200

    Docstrings + move spl_data of the profile and array of the x axis into Stationary

--- stationary.py
+++ stationary.py
@@ -2,6 +2,17 @@ import numpy as np
 
 class Stationary():
     def __init__(self, data, b, nu=5) -> None:
+        '''
+        self.N - the length of the data
+        self.b - the scalar with which the data is scaled. E.g. if b = 2, the scales considred are always 
+        half that of the previous ones. 
+        self.nu - the number of different scales considered in the analysis
+        self.s - the lengths of the segments at each scale
+        self.i - an interator variable, designating the index of the current scale array self.s.
+        It is used in R/S, FA and DFA to loop over the different scales and split the data
+        at each scale. 
+        self.x - the index of the time series data, to be used in polyfit
+        '''
         self.data = data
         self.diff_data = np.diff(self.data)
         self.diff_data_r = np.flip(self.diff_data)
@@ -10,7 +21,10 @@ class Stationary():
         self.N = self.diff_data.size
         self.s = self.N // (self.b**self.nu)
         self.i = 0
-        self.spl_data = self.split_data(self.diff_data)
+        self.spl_data = self.split_data(self.data)
+        self.spl_data_r = self.split_data(np.flip(self.data))
+        self.x = np.array(range(len(self.data)))
+        self.x_split = self.split_data(self.x)
 
     
     def split_data(self, data):
