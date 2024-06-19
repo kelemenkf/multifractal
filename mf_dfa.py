3c85e3f2833848acfeea39b425b032b59bdeda11
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-06-19 17:41:50 +0200

    fluctuation function 0

--- mf_dfa.py
+++ mf_dfa.py
@@ -13,8 +13,7 @@ class MF_DFA(DFA):
         '''
         Obtain the qth order fluctuation function
         '''
-        q_range = np.linspace(q[0], q[1], int(((q[1]-q[0]) // gran))+1)
-        print(q_range)
+        q_range = np.linspace(q[0],q[-1],int((q[1]-q[0])/gran)+1)
         fa = []
         for q in q_range:
             f_2 = np.concatenate((self.squared_fluctuation(self.spl_data, self.x_split), self.squared_fluctuation(self.spl_data_r, self.x_split_r)))
@@ -23,17 +22,28 @@ class MF_DFA(DFA):
         return fa
 
 
+    def q_fluctuation_0(self):
+        '''
+        Calculates the fluctuation function with q = 0.
+        '''
+        f_2 = np.concatenate((self.squared_fluctuation(self.spl_data, self.x_split), self.squared_fluctuation(self.spl_data_r, self.x_split_r)))
+        fa_0 = np.exp(np.sum(np.log(f_2)) / self.N_s[self.i] * 4)
+
+
     def fluctuation_functions(self):
         '''
         Calculates the fluctation function for different scales s, and for all orders q
         at each scale.
         '''
         fa_q = [self.q_fluctuation]
-        for n in range(self.nu.size):
+        for n in range(1, self.nu.size):
+            print(self.i)
             self.i += 1
             self.reset_data()
             fa_q.append(self.q_fluctuation())
         self.i = 0
         self.reset_data()
         return fa_q
+
+
 
